c4e01987f30c78415b724f4d61faf4d64527884b
rom4keee <roman_bybyk@epam.com>
2022-07-10 22:51:28 +0300

    Task_3_P_2

---
+++ src/SomethingIsWrong.java
@@ -0,0 +1,13 @@
+import java.awt.Rectangle;
+
+public class SomethingIsWrong {
+    public SomethingIsWrong() {
+    }
+
+    public static void main(String[] args) {
+        Rectangle myRect = new Rectangle();
+        myRect.width = 40;
+        myRect.height = 50;
+        System.out.println("myRect's area is " + myRect.getWidth() + myRect.getHeight());
+    }
+}
