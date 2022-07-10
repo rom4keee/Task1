ede5d068d1e8dfdaac7f2d09e63d86d3a9ef4f24
rom4keee <roman_bybyk@epam.com>
2022-07-10 22:50:13 +0300

    Task_2_P_2

---
+++ src/Computer.java
@@ -0,0 +1,42 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+public class Computer {
+    String Brand;
+    String ScreenSize;
+    int NumberOfCores;
+    String Type;
+
+    public Computer(String Brand, String ScreenSize, int NumberOfCores, String Type) {
+        this.Brand = Brand;
+        this.ScreenSize = ScreenSize;
+        this.NumberOfCores = NumberOfCores;
+        this.Type = Type;
+    }
+
+    public String getBrand() {
+        return this.Brand;
+    }
+
+    public String getScreenSize() {
+        return this.ScreenSize;
+    }
+
+    public int getNumberOfCores() {
+        return this.NumberOfCores;
+    }
+
+    public String getType() {
+        return this.Type;
+    }
+
+    public static void main(String[] args) {
+        Computer c1 = new Computer("HP", "17", 7, "Laptop");
+        System.out.println("Brand: " + c1.getBrand());
+        System.out.println("Screen size: " + c1.getScreenSize());
+        System.out.println("Number of cores: " + c1.getNumberOfCores());
+        System.out.println("Type:  " + c1.getType());
+    }
+}
