97325b073140c442515f04564c05243656ea89d8
Roman Bybyk <64772371+rom4keee@users.noreply.github.com>
2022-07-22 15:25:39 +0300

    Update Static.java

--- src/JavaOOP/Static.java
+++ src/JavaOOP/Static.java
@@ -1,11 +1,11 @@
 package JavaOOP;
-
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
 
 public class Static {
 
-    public static void main (String[] args)
-    {
+    public static void main(String[] args) {
         ArrayList<Integer> Vector = new ArrayList<Integer>();
         Vector.add(10);
         Vector.add(15);
@@ -16,11 +16,11 @@ public class Static {
         Vector.add(40);
         Vector.add(45);
         Vector.add(50);
-        for (int i : Vector){
-            System.out.println(i);
+
+        Random random = new Random();
+        for (int i = 0; i < 3; i++) {
+            int number = random.nextInt(Vector.size());
+            System.out.println(Vector.get(number));
         }
-
     }
-
 }
-
